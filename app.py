2d96575e4f95611d2af23632e872f6ab4cbc1fac
Meet Chauhan <99242018+MeetChauhan03@users.noreply.github.com>
2025-07-12 12:22:00 +0530

    2o indent

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def render_redirect_chain(chain):
         elif status_code == 'Error':
             icon = "❌"
 
-        indent = " " * (4 * i)
+        indent = " " * (20 * i)
         display += f"{indent} └─> {icon} {status_code} → `{url}`  [**{status_text}**, Server: {server}]\n"+ "\n&nbsp;"
     return display
 # === Streamlit UI ===
