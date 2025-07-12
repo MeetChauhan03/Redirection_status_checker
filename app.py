e114853c6ec26d017fc1b08b716b24fa47661059
Meet Chauhan <99242018+MeetChauhan03@users.noreply.github.com>
2025-07-12 13:24:36 +0530

    comit

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ if not urls_unique:
 # --- Check URLs with concurrency ---
 st.info(f"🔍 Checking {len(urls_unique)} unique URLs. Please wait...")
 
-results = []
+results = {}
 with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
     futures = {executor.submit(check_redirection_chain, url): url for url in urls_unique}
     for future in as_completed(futures):
